68df59f916335de8894f6789cb7ba9786b639518
Gavin Wilson <gmanwilson@icloud.com>
2024-12-14 17:38:39 -0500

    Added additional function prototypes "in_order_traversal" and "min_value_node"

--- gym_functions.h
+++ gym_functions.h
@@ -1,8 +1,11 @@
+#ifndef GYM_FUNCTION_H
+#define GYM_FUNCTION_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define MAX_LEN 100
 
+// Struct used for members in binary search tree
 struct node
 {
     char first_name[MAX_LEN + 1];
@@ -14,6 +17,12 @@ struct node
     struct node *left;
     struct node *right;
 };
+
+// Finds node with minimum value
+struct node *min_value_node(struct node *node);
+
+// Depth First Search (DFS) Algorithm to display all members
+struct node *in_order_traversal(struct node *root);
 
 // Function to create new node
 struct node *add_node(char *first_name, char *last_name, char *email, int id, int points);
@@ -48,3 +57,5 @@ void quick_subtract_1000_points(struct node *root);
 void add_points(struct node *root, int points);
 
 void subtract_points(struct node *root, int points);
+
+#endif
